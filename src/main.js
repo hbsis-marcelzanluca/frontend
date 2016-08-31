42faacb658955234a7fb76031844550901bf1ef4
hbsis-marcelzanluca <marcel.zanluca@hbsis.com.br>
2016-08-30 22:38:39 -0300

    Adicionado o React Hot Loader ao projeto

--- src/main.js
+++ src/main.js
@@ -1,2 +1,25 @@
-import cats from './cats';
-console.log(cats);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+
+class ExecutorSQL extends React.Component {
+
+	render() {
+		return (
+			<MuiThemeProvider>
+				<AppBar
+					style={{ backgroundColor: '#1E88E5' }}
+					title="Executor Teste"
+					iconClassNameRight="muidocs-icon-navigation-expand-more"
+				/>
+			</MuiThemeProvider>
+		);
+	}
+
+}
+
+ReactDOM.render(<ExecutorSQL />, document.getElementById('wrapper'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,41 @@
 const webpack = require('webpack');
 
 module.exports = {
-     entry: './src/main.js',
+     entry: {
+        app: [
+            'webpack-dev-server/client?http://localhost:8080',
+            'webpack/hot/only-dev-server',
+            './src/main.js'
+        ]
+     },
+
      output: {
          path: './bin',
          filename: 'app.bundle.js'
      },
+
       module: {
          loaders: [{
              test: /\.js$/,
              exclude: /node_modules/,
-             loader: 'babel-loader',
+             loaders: ['react-hot', 'babel-loader'],
          }]
      },
+
     plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false,
-            },
-            output: {
-                comments: false,
-            },
-        }),
-    ]
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        // new webpack.optimize.UglifyJsPlugin({
+        //     compress: {
+        //         warnings: false,
+        //     },
+        //     output: {
+        //         comments: false,
+        //     },
+        // }),
+    ],
+
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
  };
