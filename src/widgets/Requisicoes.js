ad0605b0a11d035f1b1f927505dad12ca7b0105f
hbsis-marcelzanluca <marcel.zanluca@hbsis.com.br>
2016-11-01 09:49:32 -0200

    Implementado os outros métods HTTP do wrapper para as requisições REST

--- src/widgets/Requisicoes.js
+++ src/widgets/Requisicoes.js
@@ -3,7 +3,28 @@ import ES6, { Polyfill } from 'es6-promise';
 
 export default {
 	get: (url) => { return fetch(url).then(resposta => resposta.json()); },
-	post: (url) => { return fetch(url).then(resposta => resposta.json()); },
-	put: (url) => { return fetch(url).then(resposta => resposta.json()); },
-	delete: (url) => { return fetch(url).then(resposta => resposta.json()); }
+
+	post: (url, dados) => { 
+		return fetch(url, { 
+			method: 'POST', 
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(dados)
+		}).then(resposta => resposta.json()); 
+	},
+
+	put: (url, dados) => {
+		return fetch(url, { 
+			method: 'PUT', 
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(dados)
+		}).then(resposta => resposta.json());
+	},
+
+	delete: (url) => { return fetch(url, { method: 'DELETE' }).then(resposta => resposta.json()); }
 };
